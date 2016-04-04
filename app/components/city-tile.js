82b2a12725f189018eca141a6b2d2493bedc972e
Charles Carter and Chris Houghton <charles.h.carter@gmail.com>
2016-04-04 15:12:38 -0700

    add sort function for cost of rental

--- app/components/city-tile.js
+++ app/components/city-tile.js
@@ -6,6 +6,9 @@ export default Ember.Component.extend({
     return this.get('city.name') + ', ' + this.get('city.country');
   }),
 
+  sortBy: ['cost:asc'],
+  sortedRentals: Ember.computed.sort('city.rentals', 'sortBy'),
+
   actions: {
      destroyCity(city) {
        if(confirm('Are you sure you want to destroy this city?')) {
